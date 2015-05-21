1f27cf8e879f01f4a0ed57af0f10ee83202b380f
Trekele <trekele@gmail.com>
2015-05-21 18:30:53 -0400

    Added Files Fixed all errors

--- mobile/src/main/java/com/alertuniversity/alertu/MainActivity.java
+++ mobile/src/main/java/com/alertuniversity/alertu/MainActivity.java
@@ -1,6 +1,7 @@
 package com.alertuniversity.alertu;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,8 +11,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -19,67 +22,38 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.Console;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
 
-    private SwipeRefreshLayout swipeLayout;
-    private Handler handler = new Handler();
-    private ListView theListView;
+    private ListView l;
     private ArrayList<Alert> returnArray;
+    private ListView theListView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-
-        //This is needed to have a custom action bar layout
-        final android.app.ActionBar actionBar = getActionBar();
-        actionBar.setCustomView(R.layout.action_bar);
-        actionBar.setDisplayShowTitleEnabled(false);
-        actionBar.setDisplayShowCustomEnabled(true);
-        actionBar.setDisplayUseLogoEnabled(false);
         setContentView(R.layout.activity_main);
 
+        ArrayList<Alert> alerts = new ArrayList<>();
         populateListView();
-
-        swipeLayout = (SwipeRefreshLayout) findViewById(R.id.main);
-        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                populateListView();
-                swipeLayout.setRefreshing(false);
-            }
-        });
-
-        theListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                //TODO: Display alert screen
-                /*
-                Alert idk = (Alert) adapterView.getItemAtPosition(i);
-                Intent getAlert = new Intent(MainActivity.this, DisplayAlertScreen.class);
-
-                final int result = 1;
-
-                getAlert.putExtra("Alert", idk);
-                startActivity(getAlert);
-                */
-            }
-        });
-
     }
 
     public void populateListView()
     {
         //Create a list of alerts from the server
         getTheAlerts();
-        List<Alert> alerts = returnArray;
+        ArrayList<Alert> alerts = returnArray;
 
         ListAdapter theAdapter = new MyAdapter(this, R.layout.row_layout, alerts);
         theListView = (ListView) findViewById(R.id.theListView);
@@ -104,28 +78,6 @@ public class MainActivity extends Activity {
         }
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
     private class GetAlerts extends AsyncTask<String, Void, Void>
     {
         @Override
@@ -179,7 +131,6 @@ public class MainActivity extends Activity {
                 }
                 return null;
             }
-
             catch (JSONException e)
             {
                 Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
@@ -187,4 +138,5 @@ public class MainActivity extends Activity {
             }
         }
     }
+
 }
